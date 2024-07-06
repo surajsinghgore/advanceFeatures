fa0df21b8f3060a6ae66088783e0d5532bb1d926
suraj singh rajput <surajthakurrs45@gmail.com>
2024-07-06 17:49:25 +0530

    page prevention done in  nextjs 14

--- nextjspageprevent/middleware.js
+++ nextjspageprevent/middleware.js
@@ -1,19 +1,25 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 export function middleware(request) {
-
   const Token = cookies().get("clientToken");
-
-  if (!Token) {
-    // If there's no token, redirect to the login page
-    return NextResponse.redirect(new URL("/auth/login", request.url));
+  // dashboard page prevent before login
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    if (!Token) {
+      // If there's no token, redirect to the login page
+      return NextResponse.redirect(new URL("/auth/login", request.url));
+    }
   }
 
-  // If the token exists, continue to the requested page
+  // login page prevent after login
+  if (request.nextUrl.pathname.startsWith("/auth")) {
+    if (Token) {
+      return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+    }
+  }
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/admin/:path*", "/auth/:path*"],
 };
